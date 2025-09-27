2ebff1fc614984b4129477d746fcdb0ad1c203a7
McOriginal <originalmc223@gmail.com>
2025-09-27 11:10:57 +0000

    Correction BACKEND

--- routes/AppointementRoute.js
+++ routes/AppointementRoute.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controller/UserController');
 const appointementController = require('./../controller/AppointementController');
 
 // Create a new appointment
-router.post('/createAppointment', appointementController.createAppointment);
+router.post(
+  '/createAppointment',
+  userController.authMiddleware,
+  appointementController.createAppointment
+);
 
 // Update an appointment by ID
 router.put('/updateAppointment/:id', appointementController.updateAppointment);
